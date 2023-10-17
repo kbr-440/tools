6a67fd7ee4fec56b6080e67491354f48016f5520
kbr <kra_121@hotmail.com>
2023-10-17 22:01:10 +0400

    assiment2 updated q2

--- tests/A2Q2.py
+++ tests/A2Q2.py
@@ -1,18 +1,15 @@
 from scapy.all import sniff, TCP
 
-# Define a function to print source and destination port numbers
+
 def print_ports(pkt):
     if pkt.haslayer(TCP):
         src_port = pkt[TCP].sport
         dst_port = pkt[TCP].dport
         print(f"Source Port: {src_port}, Destination Port: {dst_port}")
 
-# Specify the interface to capture packets from (replace 'eth0' with your interface)
-interface = 'eth0'
 
-# Specify the filter for capturing four HTTPS packets (TCP packets with destination port 443)
-https_filter = 'tcp and port 443'
+interface = 'eth0'  # The interface to capture packets from
 
-# Start sniffing packets and print the source and destination port numbers
+https_filter = 'tcp and port 443'  # Filter for capturing four HTTPS packets (TCP packets with destination port 443)
 print(f"Sniffing four HTTPS packets on interface {interface}...")
 sniff(iface=interface, filter=https_filter, prn=print_ports, count=4)
