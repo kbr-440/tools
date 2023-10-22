944735f9379236c16a4783b783ce71fbf057f6fe
kbr <kra_121@hotmail.com>
2023-10-22 12:53:54 +0400

    working with GUI

--- recoverytool/core.py
+++ recoverytool/core.py
@@ -1,59 +1,54 @@
 import os
-
-
-class FolderRecoveryTool:
-    def __init__(self, folder_path, output_dir="./recovered"):
-        self.folder_path = folder_path
-        self.output_dir = output_dir
-
-    def list_deleted_files(self):
-        deleted_files = []
-        for root, dirs, files in os.walk(self.folder_path):
-            for file_name in files:
-                full_path = os.path.join(root, file_name)
-                if not os.path.exists(full_path):
-                    deleted_files.append(full_path)
-        return deleted_files
-
-    def recover_file(self, file_path):
-        output_path = os.path.join(self.output_dir, os.path.basename(file_path))
-        os.makedirs(os.path.dirname(output_path), exist_ok=True)
-        with open(output_path, "wb") as out_file, open(file_path, "rb") as in_file:
-            out_file.write(in_file.read())
-
-
-def carve(folder_path, output_dir="./recovered"):
-    recovery_tool = FolderRecoveryTool(folder_path, output_dir)
-    deleted_files = recovery_tool.list_deleted_files()
-    for file_path in deleted_files:
-        recovery_tool.recover_file(file_path)
-
-
-import os
 import pytsk3
 
 
-# ... [Your existing FolderRecoveryTool class and carve function] ...
+class DiskRecoveryTool:
+
+    def __init__(self, image_path, output_dir="./recovered"):
+        self.image_path = image_path
+        self.output_dir = output_dir
+        self.img_info = pytsk3.Img_Info(self.image_path)
+        try:
+            self.fs_info = pytsk3.FS_Info(self.img_info)
+        except Exception as e:
+            raise ValueError(f"Failed to detect filesystem: {str(e)}")
+
+    def scan_for_deleted_files(self):
+        """Return a list of deleted file entries using pytsk3."""
+        deleted_files = []
+        root_directory = self.fs_info.open_dir(path="/")
+        for directory_entry in root_directory:
+            if directory_entry.info.meta and directory_entry.info.name:
+                if directory_entry.info.meta.flags & pytsk3.TSK_FS_META_FLAG_UNALLOC:
+                    deleted_files.append(directory_entry)
+        return deleted_files
+
+    def recover_file(self, directory_entry):
+        """Attempt to recover a deleted file represented by a directory entry."""
+        try:
+            file_content = directory_entry.read_random(0, directory_entry.info.meta.size)
+            output_path = os.path.join(self.output_dir, directory_entry.info.name.name.decode('utf-8'))
+            os.makedirs(os.path.dirname(output_path), exist_ok=True)
+            with open(output_path, "wb") as out_file:
+                out_file.write(file_content)
+        except Exception as e:
+            print(f"Error recovering file {directory_entry.info.name.name.decode('utf-8')}: {str(e)}")
+
+    def carve_deleted_files(self):
+        """Carve all detected deleted files."""
+        deleted_entries = self.scan_for_deleted_files()
+        for entry in deleted_entries:
+            self.recover_file(entry)
+
 
 def scan_disk_for_deleted_files(image_path):
-    # This function should return a list of deleted files using pytsk3
+    """Scan a disk image for deleted files and return their names."""
+    tool = DiskRecoveryTool(image_path)
+    deleted_entries = tool.scan_for_deleted_files()
+    return [entry.info.name.name.decode('utf-8') for entry in deleted_entries]
 
-    # Open the disk image
-    img_info = pytsk3.Img_Info(image_path)
 
-    # Try auto-detection for the filesystem
-    try:
-        fs_info = pytsk3.FS_Info(img_info)
-    except Exception as e:
-        raise ValueError(f"Failed to detect filesystem: {str(e)}")
-
-    # Logic to scan and get the list of deleted files
-    deleted_files = []
-    root_directory = fs_info.open_dir(path="/")
-    for directory_entry in root_directory:
-        if directory_entry.info.meta and directory_entry.info.name:
-            # Check if the file is deleted
-            if directory_entry.info.meta.flags & pytsk3.TSK_FS_META_FLAG_UNALLOC:
-                deleted_files.append(directory_entry.info.name.name.decode('utf-8'))
-
-    return deleted_files
+def carve_files_from_disk(image_path, output_dir="./recovered"):
+    """Scan a disk image for deleted files and attempt to recover them."""
+    tool = DiskRecoveryTool(image_path, output_dir)
+    tool.carve_deleted_files()
--- recoverytool/gui/main_window.py
+++ recoverytool/gui/main_window.py
@@ -1,5 +1,6 @@
+import os
 import tkinter as tk
-from tkinter import filedialog, messagebox
+from tkinter import filedialog, ttk, messagebox
 from recoverytool import core
 
 class MainWindow(tk.Tk):
@@ -7,42 +8,40 @@ class MainWindow(tk.Tk):
         super().__init__()
 
         self.title("Recovery Tool")
-
-        # Create and place widgets
+        self.geometry("500x400")  # Set a default size
+        self.configure(bg='light gray')  # Set the background color of the main window
         self.create_widgets()
 
     def create_widgets(self):
-        self.label = tk.Label(self, text="Select Disk Image")
-        self.label.pack(pady=20)
+        self.label = ttk.Label(self, text="Select Disk Image", background='light gray')
+        self.label.grid(row=0, column=0, pady=20, padx=20, sticky='w')
 
-        self.browse_button = tk.Button(self, text="Browse", command=self.browse_disk_image)
-        self.browse_button.pack(pady=20)
+        self.browse_button = ttk.Button(self, text="Browse", command=self.browse_disk_image)
+        self.browse_button.grid(row=1, column=0, pady=20, padx=20, sticky='w')
 
-        self.recover_button = tk.Button(self, text="Recover", command=self.recover_files)
-        self.recover_button.pack(pady=20)
+        # Display selected file name
+        self.file_label = ttk.Label(self, text="No file selected", background='light gray')
+        self.file_label.grid(row=1, column=1, pady=20, padx=20, sticky='w')
+
+        self.recover_button = ttk.Button(self, text="Recover", command=self.recover_files)
+        self.recover_button.grid(row=2, column=0, pady=20, padx=20, sticky='w')
 
     def browse_disk_image(self):
         file_path = filedialog.askopenfilename()
-        # Store file path and do additional stuff if necessary
-        self.image_path = file_path
-
+        if file_path:
+            self.image_path = file_path
+            self.file_label.config(text=os.path.basename(file_path))
 
     def recover_files(self):
-        if hasattr(self, 'image_path'):  # Check if 'image_path' attribute exists
-            deleted_files = core.scan_disk_for_deleted_files(self.image_path)
-        try:
-            if hasattr(self, 'image_path'):
-                deleted_files = core.scan_disk_for_deleted_files(self.image_pat) # Example for NTFS
-                recovered_files = core.carve_files(deleted_files)
-                core.save_recovered_files(recovered_files, "output_directory")  # Specify your output directory
-        except Exception as e:
-            # If there's an error, show it in a message box
-            messagebox.showerror("Recovery Error", f"An error occurred: {str(e)}")
+        if hasattr(self, 'image_path'):
+            try:
+                deleted_files = core.scan_disk_for_deleted_files(self.image_path)
+                core.carve_files_from_disk(self.image_path, "output_directory")
+                messagebox.showinfo("Success", "Files recovered successfully!")
+            except Exception as e:
+                messagebox.showerror("Recovery Error", f"An error occurred: {str(e)}")
         else:
-            # Handle the case where the image path has not been set.
-            # Here, we'll display an error message in the GUI prompting the user to select an image.
-            tk.messagebox.showerror("Error", "Please select a disk image before attempting recovery.")
-
+            messagebox.showerror("Error", "Please select a disk image before attempting recovery.")
 
 if __name__ == "__main__":
     app = MainWindow()
