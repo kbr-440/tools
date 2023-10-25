b6f2fd8aa11749dddccc3217afe6414fdcb74eed
kbr <kra_121@hotmail.com>
2023-10-25 15:55:45 +0400

    updated GUI updated Core to handel recovering all files and deleted files

--- recoverytool/core.py
+++ recoverytool/core.py
@@ -16,31 +16,37 @@ class DiskRecoveryTool:
         except Exception as e:
             print(f"Warning: Failed to detect filesystem: {str(e)}")
 
-    def scan_for_deleted_files(self):
-        """Return a list of deleted file entries using pytsk3."""
+    def scan_files(self, recover_all=False):
+        """Return a list of file entries using pytsk3, filtered based on user's choice."""
         if not self.fs_info:
             raise errors.FilesystemNotFoundError("Filesystem information is not available.")
-        deleted_files = []
+        files = []
         try:
             root_directory = self.fs_info.open_dir(path="/")
             for directory_entry in root_directory:
                 if directory_entry.info.meta and directory_entry.info.name:
-                    if directory_entry.info.meta.flags & pytsk3.TSK_FS_META_FLAG_UNALLOC:
-                        deleted_files.append(directory_entry)
+                    if recover_all or (directory_entry.info.meta.flags & pytsk3.TSK_FS_META_FLAG_UNALLOC):
+                        files.append(directory_entry)
         except Exception as e:
-            raise errors.FileRecoveryError(f"Error scanning for deleted files: {str(e)}") from e
-        return deleted_files
+            raise errors.FileRecoveryError(f"Error scanning for files: {str(e)}") from e
+        return files
 
     def recover_file(self, directory_entry):
         try:
+            file_name = directory_entry.info.name.name.decode('utf-8')
+
+            # Skip special files
+            if file_name.startswith("$"):
+                print(f"Skipping special file: {file_name}")
+                return
+
             file_content = directory_entry.read_random(0, directory_entry.info.meta.size)
-            output_path = os.path.join(self.output_dir, directory_entry.info.name.name.decode('utf-8'))
+            output_path = os.path.join(self.output_dir, file_name)
             os.makedirs(os.path.dirname(output_path), exist_ok=True)
             with open(output_path, "wb") as out_file:
                 out_file.write(file_content)
         except Exception as e:
-            raise errors.FileRecoveryError(
-                f"Error recovering file {directory_entry.info.name.name.decode('utf-8')}: {str(e)}") from e
+            print(f"Warning: Error recovering file {file_name}: {str(e)}")
 
     def carve_deleted_files(self):
         deleted_entries = self.scan_for_deleted_files()
@@ -50,20 +56,21 @@ class DiskRecoveryTool:
 
 
 def scan_disk_for_deleted_files(image_path):
+    print("Scanning disk...")
     tool = DiskRecoveryTool(image_path)
     deleted_entries = tool.scan_for_deleted_files()
     if "error" in deleted_entries:
+        print("Error in scanning:", deleted_entries["error"])
         return deleted_entries
     return [entry.info.name.name.decode('utf-8') for entry in deleted_entries]
 
-
 def carve_files_from_disk(image_path, output_dir="./recovered"):
+    print("Carving files...")
     tool = DiskRecoveryTool(image_path, output_dir)
     result = tool.carve_deleted_files()
     if result and "error" in result:
+        print("Error in carving:", result["error"])
         return result
+    print("Carving completed!")
     return {}
 
-
-class DiskRecoveryError(Exception):
-    """Custom exception for DiskRecoveryTool errors."""
--- recoverytool/gui/main_window.py
+++ recoverytool/gui/main_window.py
@@ -1,6 +1,6 @@
 import os
 import tkinter as tk
-from tkinter import filedialog, ttk
+from tkinter import filedialog, ttk, simpledialog, font
 from recoverytool import core
 from recoverytool import errors
 
@@ -9,33 +9,65 @@ class MainWindow(tk.Tk):
     def __init__(self):
         super().__init__()
 
+        # Fonts and Styles
+        self.main_font = font.Font(family='Arial', size=12)
+        self.error_font = font.Font(family='Arial', size=10, weight='bold')
+
+        # Styles for ttk widgets
+        self.style = ttk.Style()
+        self.style.configure("TLabel", font=self.main_font)
+        self.style.configure("TButton", font=self.main_font)
+
+        # Main Window Configuration
         self.title("Recovery Tool")
-        self.geometry("500x400")  # Set a default size
-        self.configure(bg='light gray')  # Set the background color of the main window
+        self.geometry("600x450")
+        self.configure(bg='#2e2e2e')  # Dark background color
         self.create_widgets()
 
     def create_widgets(self):
-        self.label = ttk.Label(self, text="Select Disk Image", background='light gray')
-        self.label.grid(row=0, column=0, pady=20, padx=20, sticky='w')
+        # Frame for selecting the image
+        image_frame = ttk.Frame(self)
+        image_frame.grid(row=0, column=0, padx=20, pady=20, sticky='w')
 
-        self.browse_button = ttk.Button(self, text="Browse", command=self.browse_disk_image)
-        self.browse_button.grid(row=1, column=0, pady=20, padx=20, sticky='w')
+        # Disk Image Widgets
+        ttk.Label(image_frame, text="Select Disk Image:").grid(row=0, column=0, padx=5, pady=5)
+        self.browse_button = ttk.Button(image_frame, text="Browse", command=self.browse_disk_image)
+        self.browse_button.grid(row=0, column=1, padx=5, pady=5)
+        self.file_label = ttk.Label(image_frame, text="No file selected", wraplength=300)
+        self.file_label.grid(row=1, column=0, columnspan=2, padx=5, pady=5)
 
-        # Display selected file name
-        self.file_label = ttk.Label(self, text="No file selected", background='light gray')
-        self.file_label.grid(row=1, column=1, pady=20, padx=20, sticky='w')
+        # Frame for output directory
+        output_frame = ttk.Frame(self)
+        output_frame.grid(row=1, column=0, padx=20, pady=20, sticky='w')
 
-        # Error message label
-        self.error_text = tk.Text(self, height=3, width=40, wrap=tk.WORD, fg='red', bg='light gray', borderwidth=0)
-        self.error_text.grid(row=2, column=1, pady=10, padx=20, sticky='w')
-        self.error_text.config(state=tk.DISABLED)  # Disable editing
+        # Output Directory Widgets
+        ttk.Label(output_frame, text="Output Directory:").grid(row=0, column=0, padx=5, pady=5)
+        self.output_dir_button = ttk.Button(output_frame, text="Choose", command=self.choose_output_dir)
+        self.output_dir_button.grid(row=0, column=1, padx=5, pady=5)
+        self.selected_output_dir_label = ttk.Label(output_frame, text="Default: ./recovered")
+        self.selected_output_dir_label.grid(row=1, column=0, columnspan=2, padx=5, pady=5)
 
+        # Options and Recovery Button
+        self.recover_all_var = tk.IntVar()
+        self.recover_all_checkbutton = tk.Checkbutton(self, text="Recover all files", variable=self.recover_all_var,
+                                                      bg='#2e2e2e', fg='white')
+        self.recover_all_checkbutton.grid(row=2, column=0, padx=20, pady=10, sticky='w')
         self.recover_button = ttk.Button(self, text="Recover", command=self.recover_files)
-        self.recover_button.grid(row=3, column=0, pady=20, padx=20, sticky='w')
+        self.recover_button.grid(row=2, column=0, padx=20, pady=10, sticky='e')
 
-        # Create an error_label widget
-        self.error_label = ttk.Label(self, text="", background='light gray', foreground='red')
-        self.error_label.grid(row=3, column=1, pady=10, padx=20, sticky='w')
+        # Error message display
+        self.error_text = tk.Text(self, height=3, width=50, wrap=tk.WORD, fg='red', bg='#2e2e2e', borderwidth=0)
+        self.error_text.grid(row=3, column=0, padx=20, pady=10, sticky='w')
+        self.error_text.config(state=tk.DISABLED)
+
+        # Applying Fonts
+        for child in self.winfo_children():
+            widget_type = child.winfo_class()
+            if widget_type in ("Label", "Checkbutton", "Text", "Button"):
+                child.configure(font=self.main_font)
+            elif isinstance(child, tk.Text):
+                child.configure(font=self.error_font)
+
 
     def browse_disk_image(self):
         file_path = filedialog.askopenfilename()
@@ -43,6 +75,12 @@ class MainWindow(tk.Tk):
             self.image_path = file_path
             self.file_label.config(text=os.path.basename(file_path))
             self.error_label.config(text="")  # Clear any previous errors
+
+    def choose_output_dir(self):
+        dir_path = filedialog.askdirectory()
+        if dir_path:
+            self.output_dir = dir_path
+            self.selected_output_dir_label.config(text=dir_path)
 
     def clear_error_text(self):
         self.error_text.config(state=tk.NORMAL)  # Enable editing
@@ -55,13 +93,28 @@ class MainWindow(tk.Tk):
         self.error_text.config(state=tk.DISABLED)  # Disable editing
 
     def recover_files(self):
+        # Clear the error text at the start
+        self.clear_error_text()
+
         if hasattr(self, 'image_path'):
-            try:
-                result = core.scan_disk_for_deleted_files(self.image_path)
-                core.carve_files_from_disk(self.image_path, "output_directory")
-                self.set_error_text("Files recovered successfully!")
-            except errors.DiskRecoveryError as e:  # Catch any error derived from DiskRecoveryError
-                self.set_error_text(str(e))
+            # Determine which files to recover based on user's choice:
+            files_to_recover = core.DiskRecoveryTool(self.image_path).scan_files(recover_all=self.recover_all_var.get())
+
+            # Optional: You can check the length of `files_to_recover` and display some messages.
+            if not files_to_recover:
+                self.set_error_text("No files found for recovery.")
+                return
+
+            output_path = getattr(self, 'output_dir', './recovered')
+
+            # Create an instance of your recovery tool
+            recovery_tool = core.DiskRecoveryTool(self.image_path, output_path)
+
+            # Recover each file
+            for file_entry in files_to_recover:
+                recovery_tool.recover_file(file_entry)
+
+            self.set_error_text("Files recovered successfully!")
         else:
             self.set_error_text("Please select a disk image before attempting recovery.")
 
